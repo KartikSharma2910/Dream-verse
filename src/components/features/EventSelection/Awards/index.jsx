eaaee355157ab0923023451570ceba3ccdc898b9
KartikSharma2910 <kartiksharma200029@gmail.com>
2024-05-13 13:09:33 +0530

    New changes

--- src/components/features/EventSelection/Awards/index.jsx
+++ src/components/features/EventSelection/Awards/index.jsx
@@ -1,5 +1,8 @@
 import { Box } from "@mui/material";
 import { Button, Section, VideoPlayer } from "components/common";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import styles from "./styles";
 
 const Awards = () => {
@@ -7,56 +10,67 @@ const Awards = () => {
     <Section customStyles={styles.wrapper}>
       <Box sx={styles.container}>
         <Box sx={styles.box}>
-          <Box
-            sx={{
-              width: "33%",
-              height: "100%",
-              border: "1px solid red",
-              background: "#06060E66",
-            }}
-          >
-            <Box
-              sx={{
-                width: "100px",
-                height: "100px",
-                borderRadius: "50%",
-              }}
-            >
+          <Box sx={styles.upperBoxOne}>
+            <Box sx={styles.userImageWrapper}>
               <Box
                 component="img"
                 src="/eventThumblain.png"
-                sx={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "50%",
-                  objectFit: "cover",
-                }}
+                sx={styles.userImage}
               />
             </Box>
-            <Box>
-              <Box>Riya Sharma</Box>
-              <Box>travel enthusiasts</Box>
+            <Box sx={styles.userTextWrapper}>
+              <Box sx={styles.userName}>Riya Sharma</Box>
+              <Box sx={styles.userPost}>travel enthusiasts</Box>
             </Box>
-            <Button as="GradientButton" label="Vote Now" />
-          </Box>
-          <Box
-            sx={{
-              width: "33%",
-              height: "100%",
-            }}
-          >
-            <VideoPlayer
-              thumbnail="/eventThumblain.png"
-              src="https://www.taxmann.com/emailer/images/FEMA.mp4"
+            <Box sx={styles.socialContainer}>
+              <Box sx={styles.iconWrapper}>
+                <YouTubeIcon />
+              </Box>
+              <Box sx={styles.iconWrapper}>
+                <InstagramIcon />
+              </Box>
+              <Box sx={styles.iconWrapper}>
+                <TwitterIcon />
+              </Box>
+            </Box>
+            <Button
+              as="GradientButton"
+              label="Vote Now"
+              customStyles={styles.button}
             />
           </Box>
-          <Box
-            sx={{
-              width: "33%",
-              height: "100%",
-              border: "1px solid green",
-            }}
-          ></Box>
+          <Box sx={styles.upperBoxTwo}>
+            <Box component="img" src="/video.png" sx={styles.videoImage} />
+          </Box>
+          <Box sx={styles.upperBoxThree}>
+            <Box sx={styles.dataWrapper}>
+              <Box sx={styles.dataHeading}>Bio</Box>
+              <Box>
+                Hi, I'm Riya! A passionate travel vlogger with a knack for
+                storytelling and adventure. Join me as I explore the world one
+                destination at a time.
+              </Box>
+            </Box>
+            <Box sx={styles.dataLowerWrapper}>
+              <Box sx={styles.dataLowerOne}>
+                <Box
+                  component="img"
+                  src="/textBackground.png"
+                  sx={styles.dataLowerImage}
+                />
+              </Box>
+              <Box sx={styles.dataLowerTwo}>
+                <Box>
+                  <Box>Subscriber</Box>
+                  <Box sx={styles.designText}>230641+</Box>
+                </Box>
+                <Box>
+                  <Box>Watch hour</Box>
+                  <Box sx={styles.designText}>980008+</Box>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
         </Box>
         <Box sx={styles.box}>
           <Box
--- src/components/features/EventSelection/Awards/styles.js
+++ src/components/features/EventSelection/Awards/styles.js
@@ -11,9 +11,151 @@ const styles = {
 
   box: {
     display: "flex",
+    gap: 3,
+    padding: "2%",
     justifyContent: "space-between",
+  },
+
+  upperBoxOne: {
+    gap: 3,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "33%",
+    height: "400px",
+    background: "#06060E66",
+  },
+
+  userImageWrapper: {
+    width: "130px",
+    height: "130px",
+    borderRadius: "50%",
+  },
+
+  userImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: "50%",
+    objectFit: "cover",
+  },
+
+  userTextWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "10px",
+  },
+
+  userName: {
+    fontSize: "22px",
+    fontWeight: 600,
+  },
+
+  userPost: {
+    background: "linear-gradient(90deg, #8E4FFF 0%, #F722C3 100%)",
+    "-webkit-background-clip": "text",
+    "-webkit-text-fill-color": "transparent",
+    textTransform: "uppercase",
+    letterSpacing: "2px",
+  },
+
+  socialContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+
+  videoImage: {
+    width: "100%",
+    height: "100%",
+    objectFit: "contain",
+  },
+
+  iconWrapper: {
+    padding: "10px",
     border: "1px solid white",
-    // height: "200px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+    width: "30%",
+    borderRadius: "12px",
+  },
+
+  button: {
+    fontSize: "22px",
+    fontWeight: "bold",
+    width: "100%",
+  },
+
+  upperBoxTwo: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "33%",
+    height: "400px",
+  },
+
+  upperBoxThree: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 2,
+    width: "33%",
+    height: "400px",
+  },
+
+  dataWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: "15px",
+    padding: "2%",
+    borderRadius: "12px",
+    border: "1px solid #999999",
+  },
+
+  dataHeading: {
+    fontSize: "22px",
+    fontWeight: 600,
+  },
+
+  dataLowerWrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+  dataLowerOne: {
+    width: "48%",
+    height: "100%",
+  },
+
+  dataLowerImage: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
+
+  dataLowerTwo: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "48%",
+    height: "100%",
+    padding: "2%",
+    borderRadius: "12px",
+    border: "1px solid #999999",
+    background: "#06060E66",
+  },
+
+  designText: {
+    fontSize: "28px",
+    background: "linear-gradient(90deg, #8E4FFF 0%, #F722C3 100%)",
+    "-webkit-background-clip": "text",
+    "-webkit-text-fill-color": "transparent",
   },
 };
 
